a426c7bbdbe864816cfb7867438b56402dedc390
kalmonJ <kalmonkk69@gmail.com>
2023-01-06 22:23:51 -0300

    fix: clear console

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -3,7 +3,6 @@ import cors from "cors";
 const allowedOrigins = ["http://localhost:3000", "http://DESKTOP-N83EHF3:3000"];
 const corsOptionsDelegate = (req: any, callback: any) => {
   let corsOptions;
-  console.log(req.header("Origin"));
   if (allowedOrigins.indexOf(req.header("Origin")) !== -1) {
     corsOptions = { origin: true };
   } else {
